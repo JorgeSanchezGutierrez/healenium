b3add6c84db696c7e2dbd0519928367ff8e6f55d
JorgeSanchezGutierrez <121303937+JorgeSanchezGutierrez@users.noreply.github.com>
2024-09-11 13:05:37 -0300

    Add files via upload

--- reporte_healenium.py
+++ reporte_healenium.py
@@ -10,7 +10,7 @@ from datetime import datetime, timedelta
 import reporte_healenium_param
 
 print("")
-print("Espere por favor, procesando...")
+print("Espere por favor...")
 
 # Obtener la fecha y hora actual
 fecha_hora_actual = datetime.now()
@@ -22,7 +22,7 @@ nueva_fecha_hora = fecha_hora_actual - timedelta(minutes=minutos_a_restar)
 # print(f"Fecha y hora después de restar {minutos_a_restar} minutos: {nueva_fecha_hora}")
 # Fecha Hora actual
 
-
+# AREA DE RUTINAS ****************************************************************************************************#
 def parametros():
     texto01 = "\n"
 
@@ -32,9 +32,16 @@ def parametros():
     texto01 = texto01 + reporte_healenium_param.gloTipoEnvioNoHay + "<br>"
     texto01 = texto01 + "- " + reporte_healenium_param.gloTipoEnvioNoHayMsg + "<br>"
     texto01 = texto01 + "" + "<br>"
+
     texto01 = texto01 + reporte_healenium_param.gloMinutosAtrasDesdeMsg + "<br>"
     if reporte_healenium_param.gloMinutosAtrasDesde > 0:
         texto01 = texto01 + "- Fecha Hora Desde Filtro: " + str(nueva_fecha_hora) + " horas.<br>"
+
+    texto01 = (texto01 + "- Total seleccionados por Grupo correo: " +
+               str(reporte_healenium_param.gloTotalGrupoCorreo) + ".<br>")
+    texto01 = texto01 + "- Total seleccionados por Grupo correo: "
+    texto01 = texto01 + reporte_healenium_param.gloTotalGrupoCorreoMsg + "<br>"
+
     texto01 = texto01 + "-" * 70 + "<br>"
     texto01 = texto01 + ""
 
@@ -42,6 +49,8 @@ def parametros():
 
 
 def fecha_hora():
+    global xFechaHoraHoy_reporte
+
     # Fecha y Hora del dia
     fecha_hora_actual1 = datetime.now()
     xDia = str(fecha_hora_actual1.strftime("%d"))
@@ -51,17 +60,16 @@ def fecha_hora():
     xMM = str(fecha_hora_actual1.strftime("%M"))
     xSS = str(fecha_hora_actual1.strftime("%S"))
     xFH = xAno + "/" + xMes + "/" + xDia + " " + xHH + ":" + xMM + ":" + xSS + " horas"
+    xFechaHoraHoy_reporte = xAno + xMes + xDia + "_" + xHH + xMM + xSS
     return xFH
     # Fin
 
 
-granURL = ""
-granContador = 0
-granContadorProgress = 0
 def procesar_jason(xjson1):
     global granURL
     global granContador
     global granContadorProgress
+
     xsalida = ""
 
     # JSON proporcionado
@@ -83,11 +91,10 @@ def procesar_jason(xjson1):
         xsalida = xsalida + "\n"
         if i > 0:
             xsalida = xsalida + "\n"
-	
-        granContador = granContador + 1
+
+        granContador += 1
 
         buscar = "progressbar"
-
         if buscar in failed_locator['value']:
            granContadorProgress += 1
 
@@ -103,19 +110,28 @@ def procesar_jason(xjson1):
         granURL = screenshot_path
         url = f"file:///{file_path.replace(' ', '%20')}"  # Formato de URL para la ruta de archivo
         xsalida = xsalida + f"      - Path de la imagen con cambios (Ver elemento con marco en ROJO):<br>      {url}"
-	
+
     return xsalida
 
 
 def enviar_correo(contenido_html1, imagenes_rutas1):
+    global contador4
+    global contador0_envios
+    global xFechaHoraHoy
+
     # Configura la información del correo
-    sender_email = "reportehealenium@gmail.com"
+    sender_email = "soporte.ti@testgroup.cl"
     receiver_email = reporte_healenium_param.gloDestinatarios
     password = reporte_healenium_param.gloClaveApp
 
+    # Indica Grupo de Correo
+    grupocorreo = ""
+    if reporte_healenium_param.gloTotalGrupoCorreo > 0:
+        grupocorreo = "[" + str(contador4) + " de " + str(int(contador0_envios)) + "] "
+
     # Crea el mensaje
     message = MIMEMultipart("alternative")
-    message["Subject"] = "Reporte Healenium " + fecha_hora()
+    message["Subject"] = grupocorreo + "Reporte Healenium " + xFechaHoraHoy
     message["From"] = sender_email
     message["To"] = ', '.join(receiver_email)
 
@@ -129,13 +145,13 @@ def enviar_correo(contenido_html1, imagenes_rutas1):
     message.attach(msg_html)
 
     # Agregar las imágenes con el identificador cid correspondiente
-   # for i, img_ruta in enumerate(imagenes_rutas1):
-   #     if img_ruta != "***":
-   #         with open(img_ruta, 'rb') as img_file:
-   #             img = MIMEImage(img_file.read())
-   #             img.add_header('Content-ID', f'<imagen{i}>')  # 'cid' que referencia la imagen en el HTML
-   #             img.add_header('Content-Disposition', 'inline', filename=img_ruta)
-   #             message.attach(img)
+    for ii, img_ruta in enumerate(imagenes_rutas1):
+        if img_ruta != "***":
+            with open(img_ruta, 'rb') as img_file:
+                img = MIMEImage(img_file.read())
+                img.add_header('Content-ID', f'<imagen{ii}>')  # 'cid' que referencia la imagen en el HTML
+                img.add_header('Content-Disposition', 'inline', filename=img_ruta)
+                message.attach(img)
 
     # Envía el correo usando el servidor SMTP de Gmail
     try:
@@ -143,11 +159,55 @@ def enviar_correo(contenido_html1, imagenes_rutas1):
             server.login(sender_email, password)
             server.sendmail(sender_email, receiver_email, message.as_string())
             print("Correos enviado con éxito")
+            print("-------------------------\n")
     except Exception as e:
         print(f"Error al enviar el correo: {e}")
 
 
-# Configuración de la conexión
+def inicia_envio_correos():
+    global contenido_html
+    global mensajeenvio
+    global contador3
+    global contador4
+
+    contenido_html += "</body></html>"
+
+    if contador2 == 0:
+        if reporte_healenium_param.gloTipoEnvioNoHay == "N":
+            mensajeenvio = "No hay registros seleccionados, NO se envía correo."
+
+    if mensajeenvio == "OK":
+        if contador2 == 0:
+            contenido_html += "*** No hay Cambios realizados por Healenium.<br>"
+
+        texto1a = "-" * 70 + "<br><br>"
+        contenido_html += f"<p>{texto1a}</p>"
+
+        contenido_html += f"Total de registros leidos . . . : {contador1}"
+        contenido_html += "<br>" + f"Total de registros seleccionados: {contador2}"
+        contenido_html += "<br>" + f"Total de cambios realizados . . : {granContador}"
+        contenido_html += "<br>" + f"Total de cambios progress . . . : {granContadorProgress}<br><br>"
+
+        contador4 += 1
+
+        grupocorreos = ""
+        if reporte_healenium_param.gloTotalGrupoCorreo > 0:
+            grupocorreos = " " + str(contador4) + " de " + str(int(contador0_envios))
+
+        print("Espere por favor, enviando correo" + grupocorreos + "...")
+        print(reporte_healenium_param.gloDestinatarios)
+        contenido_html += parametros()
+        enviar_correo(contenido_html, imagenes)
+    else:
+        print(mensajeenvio)
+
+    contador3 = 0
+# FIN AREA DE RUTINAS ************************************************************************************************#
+
+
+# CUERPO DEL PROGRAMA PYTHON *****************************************************************************************#
+
+# Configuración de la conexión con Healenium
 conexion = psycopg2.connect(
     host=reporte_healenium_param.gloHost,
     database=reporte_healenium_param.gloDatabase,
@@ -164,17 +224,55 @@ cursor.execute("SELECT create_date, elements, uid FROM healenium.report;")
 # Obtener los resultados
 resultados = cursor.fetchall()
 
+totalPorLeer = len(resultados)
+print("Se procesaran: " + str(totalPorLeer) + " registros...\n")
+
 # Inicializar variables
+granURL = ""
+granContador = 0
+granContadorProgress = 0
+
 imagenes = []
 contenido_html = "<html><body>"
 
+contador0 = 0
+contador0_envios = 0
+
 contador1 = 0
 contador2 = 0
-texto = "Reporte de cambios en la ejecución de automatizaciones.<br><br>"
-contenido_html += texto
-texto = "-" * 70 + "<br>"
-contenido_html += texto
-mensajeenvio = "OK"
+contador3 = 0
+contador4 = 0
+mensajeenvio = ""
+
+xFechaHoraHoy_reporte = ""
+xFechaHoraHoy = fecha_hora()
+
+# Contar seleccionados en forma previa
+if reporte_healenium_param.gloTotalGrupoCorreo > 0:
+    print("Obteniendo total de seleccionados en forma previa...\n")
+    for fila in resultados:
+        create_date, elements, uid = fila
+
+        xseguir1 = "N"
+
+        if elements != '{"records":[]}':
+            xseguir1 = "S"
+
+        if xseguir1 == "S":
+            if reporte_healenium_param.gloMinutosAtrasDesde > 0:
+                if create_date < nueva_fecha_hora:
+                    xseguir1 = "N"
+
+        if xseguir1 == "S":
+            contador0 += 1
+
+    contador0_envios = contador0 / reporte_healenium_param.gloTotalGrupoCorreo
+
+    resto = contador0 % reporte_healenium_param.gloTotalGrupoCorreo
+    if resto > 0:
+        contador0_envios = int(contador0_envios)
+        contador0_envios += 1
+# Fin Contar seleccionados
 
 # Generar contenido del correo
 for fila in resultados:
@@ -193,6 +291,16 @@ for fila in resultados:
                 xseguir = "N"
 
     if xseguir == "S":
+        contador3 += 1
+        if contador3 == 1:
+            imagenes = []
+            contenido_html = "<html><body>"
+            texto = "Reporte de cambios en la ejecución de automatizaciones.<br><br>"
+            contenido_html += texto
+            texto = "-" * 70 + "<br>"
+            contenido_html += texto
+            mensajeenvio = "OK"
+
         contador2 += 1
 
         texto1 = texto1 + f"Registro seleccionado #{contador2}" + "<br>"
@@ -208,40 +316,20 @@ for fila in resultados:
 
         contenido_html += f"<p>{texto1}</p>"
         # Agregar la imagen justo debajo del texto
-       # i = contador2 - 1
-       # if i < len(imagenes):  # Asegura que no exceda la cantidad de imágenes
-       #     contenido_html += f'<br><img src="cid:imagen{i}" alt="*********************************************"><br>'
+        i = contador3 - 1
+        if i < len(imagenes):  # Asegura que no exceda la cantidad de imágenes
+            contenido_html += f'<br><img src="cid:imagen{i}" alt="*********************************************"><br>'
+    if reporte_healenium_param.gloTotalGrupoCorreo > 0:
+        if contador3 == reporte_healenium_param.gloTotalGrupoCorreo:
+            inicia_envio_correos()
 
-contenido_html += "</body></html>"
+if contador3 > 0:
+    inicia_envio_correos()
 
-if contador2 == 0:
-    if reporte_healenium_param.gloTipoEnvioNoHay == "N":
-        mensajeenvio = "No hay registros seleccionados, NO se envía correo."
-
-if mensajeenvio == "OK":
-    if contador2 == 0:
-        contenido_html += "*** No hay Cambios realizados por Healenium.<br>"
-
-    texto1 = "-" * 70 + "<br><br>"
-    contenido_html += f"<p>{texto1}</p>"
-
-    contenido_html += f"Total de registros leidos . . . : {contador1}"
-    contenido_html += "<br>" + f"Total de registros seleccionados: {contador2}<br><br>"
-
-    print("")
-    print("Espere por favor, enviando correos...")
-    print(reporte_healenium_param.gloDestinatarios)
-    # print("Enviando correo a: " + reporte_healenium_param.gloDestinatarios)
-    contenido_html += parametros()
-    enviar_correo(contenido_html, imagenes)
-else:
-    print(mensajeenvio)
-
-print("")
 print(f"Total de registros leidos . . . : {contador1}")
 print(f"Total de registros seleccionados: {contador2}")
-print(f"Total de cambios realizados: {granContador}")
-print(f"Total de cambios progress: {granContadorProgress}")
+print(f"Total de cambios realizados . . : {granContador}")
+print(f"Total de cambios progress . . . : {granContadorProgress}")
 
 # FIN
 
--- reporte_healenium_param.py
+++ reporte_healenium_param.py
@@ -1,25 +1,28 @@
 # Para envío de correos
-gloDestinatarios = ["nelson.colman@testgroup.cl", "jeremy.burgos@testgroup.cl", "reportehealenium@gmail.com",
-                    "katherine.andrade@testgroup.cl", "ext-martha.hernandez@sbins.cl"]
-gloClaveApp = "eocd vcyg angl vmro"
+gloDestinatarios = ["nelson.colman@testgroup.cl", "jorge.sanchez@testgroup.cl"]
+gloClaveApp = "dyuc aaiz mnfd ownt"
 
-# Tipo de envio email
+# Tipo de envio email 
 gloTipoEnvioNoHay = "S"
 gloTipoEnvioNoHayMsg = "Envio de correos Si No Hay registros seleccionados: "
 gloTipoEnvioNoHayMsg = gloTipoEnvioNoHayMsg + "S=Aunque NO haya registros seleccionados, el correo se envía igual. "
 gloTipoEnvioNoHayMsg = gloTipoEnvioNoHayMsg + "N=Si NO hay registros seleccionados, el correo no se envía."
 
 # Filtro de Fecha y Hora
-gloMinutosAtrasDesde = 0  # 24 horas, cambiar por 0 para revisión total 
+gloMinutosAtrasDesde = 0
 gloMinutosAtrasDesdeMsg = "- Minutos atrás Desde para revisar cambios: " + str(gloMinutosAtrasDesde) + " minutos"
-gloMinutosAtrasDesdeMsg = gloMinutosAtrasDesdeMsg + " (Mayor a 0 = Minutos Desde, 0=No se filtra por Fecha)."
+gloMinutosAtrasDesdeMsg = gloMinutosAtrasDesdeMsg + " (0: No se filtra por Fecha / Mayor a 0: Minutos Desde)."
+
+# Grupo de Correos, es decir, cada cuantos seleccionados se envía un correos
+gloTotalGrupoCorreo = 0
+gloTotalGrupoCorreoMsg = "0: Se genera 1 correo con Todos los cambios seleccionados "
+gloTotalGrupoCorreoMsg = gloTotalGrupoCorreoMsg + "/ Valor Mayor a 0: Se genera 1 correo para cada grupo indicado."
 
 # Para conexion a la Base de Datos
-gloHost = "192.168.40.17"  # Ej. "localhost" si está en tu máquina local -> IP del servidor
+gloHost = "localhost"  # Ej. "localhost" si está en tu máquina local
 gloDatabase = "healenium"
 gloUser = "healenium_user"
 gloPassword = "YDk2nmNs4s9aCP6K"
-#datos Data base 
 
 # Ruta origen de Docker-compose
-gloRutaCompose = "/healenium"
+gloRutaCompose = "C:/zHealenium/healenium-2.0.0/healenium-2.0.0"
